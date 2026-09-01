591932de73209a59769181ec64961824d777d13e
Andy <93466499+Dackkersen@users.noreply.github.com>
2026-09-01 17:44:44 +0300

    feat: network handling for currency and currency updating

--- Source/ReplicatedStorage/Modules/Network.lua
+++ Source/ReplicatedStorage/Modules/Network.lua
@@ -3,7 +3,11 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Bolt = require(ReplicatedStorage:WaitForChild("Libraries"):WaitForChild("Bolt"))
 
 local Network = {
-    Money = Bolt.RemoteProperty("Money", 0) :: Bolt.RemoteProperty<number>,
+	Money = Bolt.RemoteProperty("Money", 0) :: Bolt.RemoteProperty<number>,
+	Speed = Bolt.RemoteProperty("Speed", 0) :: Bolt.RemoteProperty<number>,
+	Strength = Bolt.RemoteProperty("Strength", 0) :: Bolt.RemoteProperty<number>,
+
+	CurrencyUpdated = Bolt.ReliableEvent("CurrencyUpdated") :: Bolt.ReliableEvent<>,
 }
 
 return Network
